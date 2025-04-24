62d97925bb1e6d33ee01949fdf9b70e39e277cb3
wahidkhan7 <iwahidkhan786@gmail.com>
2025-04-24 17:00:43 +0530

    upto 4:31 done

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashboardLayout from '../../components/layout/DashboardLayout'
 import useUserAuth from '../../hooks/useUserAuth'
+import { useNavigate } from 'react-router-dom'
+import HeaderWithFilter from '../../components/layout/HeaderWithFilter'
 
 
 const Home = () => {
     useUserAuth()
+    const navigate = useNavigate()
+    const[allPolls,setAllPolls]=useState([])
+    const[stats,setStats]= useState([])
+    const[page,setPage]=useState(1)
+    const[hasMore,setHasMore]=useState(true)
+    const[loading,setLoading]=useState(false)
+
+    const[filterType,setFilterType]=useState("")
+
+    const fetchAllPolls = async(overridgePage = page)=>{
+      if(loading) return;
+
+      setLoading(true)
+
+      try {
+        
+      } catch (error) {
+        
+      }
+      finally{
+        
+      setLoading(false)
+
+      }
+
+    }
 
   return (
   
     <DashboardLayout activeMenu = 'Dashboard'>  
-      <div>Home</div>
+      <div className='my-5 mx-auto'>
+        <HeaderWithFilter
+        title ="Polls"
+        filterType ={filterType}
+        setFilterType={setFilterType}
+        />
+
+      </div>
     </DashboardLayout>
   )
 }
