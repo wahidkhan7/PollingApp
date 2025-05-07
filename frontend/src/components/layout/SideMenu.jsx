7b39ff9a69cb5cf669816d95e142555e8717e14a
wahidkhan7 <iwahidkhan786@gmail.com>
2025-05-08 01:31:37 +0530

    ....

--- frontend/src/components/layout/SideMenu.jsx
+++ frontend/src/components/layout/SideMenu.jsx
@@ -24,15 +24,7 @@ const SideMenu = ({activeMenu}) => {
 
   return (
     <div className='w-64 h-[calc(100vh-61px)] bg-slate-50/50 border-r border-slate-100/70 p-5 sticky top-[61px] z-20'>
-        {/* {SIDE_MENU_DATA.map((item,index)=>{
-            <button
-                key={`menu_${index}`}
-                className={`w-full flex items-center gap-4 text-[15px] ${
-                    activeMenu == item.label?"text-white bg-primary" : ""
-                } py-4 px-6 rounded-full mb-3`}
-                onClick={()=>handleClick(item.path)}
-            ><item.icon className='text-xl'/> {item.label}</button>
-        })} */}
+      
         {SIDE_MENU_DATA.map((item, index) => (
              <button
             key={`menu_${index}`}
